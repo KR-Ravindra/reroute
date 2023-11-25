6809706fe095c2fa9f9bc4093e2c2fad31b66ee8
Ravindra <42912207+KR-Ravindra@users.noreply.github.com>
2023-11-24 22:23:59 -0800

    Things are being simulated now

--- src/components/Home/replot.js
+++ src/components/Home/replot.js
@@ -1,13 +1,45 @@
+import { Permutation } from 'js-combinatorics';
+import haversine from 'haversine-distance';
+
 export function animateData(setData) {
-    // Set initial data (Ohio to Los Angeles)
-    setData([
-      { sourcePosition: [-82.9988, 39.9612], targetPosition: [-118.2437, 34.0522] }
-    ]);
-  
-    // After one second, change destination to San Francisco
+  setTimeout(() => {
+    setData([]);
+})
+  const locations = [
+    { position: [-82.9988, 39.9612] }, // Ohio
+    { position: [-118.2437, 34.0522] }, // Los Angeles
+    { position: [-122.41669, 37.7853] }, // San Francisco
+    { position: [-74.0060, 40.7128] }, // New York
+    { position: [-87.6298, 41.8781] } // Chicago
+  ];
+
+  const calculateDistance = (location1, location2) => {
+    return haversine(location1.position, location2.position);
+  };
+
+  const permutations = new Permutation(locations.slice(1)).toArray();
+  permutations.forEach(permutation => permutation.unshift(locations[0]));
+
+  let shortestDistance = Infinity;
+  let shortestRoute = null;
+
+  permutations.forEach(permutation => {
+    let totalDistance = 0;
+    for (let i = 0; i < permutation.length - 1; i++) {
+      totalDistance += calculateDistance(permutation[i], permutation[i + 1]);
+    }
+    if (totalDistance < shortestDistance) {
+      shortestDistance = totalDistance;
+      shortestRoute = permutation;
+    }
+  });
+
+  shortestRoute.forEach((location, i) => {
     setTimeout(() => {
-      setData([
-        { sourcePosition: [-82.9988, 39.9612], targetPosition: [-122.41669, 37.7853] }
+      setData(prevData => [
+        ...prevData,
+        { sourcePosition: location.position, targetPosition: shortestRoute[i + 1]?.position || location.position }
       ]);
-    }, 1000);
-  }
+    }, i * 1000);
+  });
+}
